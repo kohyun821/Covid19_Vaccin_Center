0b4e7153ba2a4f549115dcc69968e1e57a351d76
김도현 <kohyun821@naver.com>
2023-05-04 20:42:20 +0900

    네이버 Map API적용

--- app/src/main/java/com/example/covid19_vaccin_center/Map/view/MapActivity.kt
+++ app/src/main/java/com/example/covid19_vaccin_center/Map/view/MapActivity.kt
@@ -2,11 +2,16 @@ package com.example.covid19_vaccin_center.Map.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.covid19_vaccin_center.BuildConfig
 import com.example.covid19_vaccin_center.R
+import com.naver.maps.map.NaverMapSdk
 
 class MapActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
+
+        NaverMapSdk.getInstance(this).client =
+            NaverMapSdk.NaverCloudPlatformClient(BuildConfig.CLIENT_ID)
     }
 }
